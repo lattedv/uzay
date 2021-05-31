daadb6a848307f00f59da7c9bb53c5fe9be82f96
Glitch (glitch-hello-node) <none>
2021-05-31 12:29:47 +0000

    🚪🍨 Checkpoint ./events/ready.js:23277224/101 ./util/eventLoader.js:23277224/19 ./komutlar/ping.js:23277224/18 ./ayarlar.json:23277224/53 ./README.md:23277224/3427 ./.gitignore:23277224/12 ./komutlar/yardım.js:23277224/726

--- events/ready.js
+++ events/ready.js
@@ -12,5 +12,9 @@ module.exports = client => {
   client.user.setStatus("dnd");
   //idle : Boşta dnd : rahatsız Etmeyin online : çevrimiçi
  client.user.setActivity("SANCTUS <3 SİZ", { type: "WATCHING" });
+  // PLAYING : oynuyur LISTENING : dinliyor  WATCHING : İZLİOR 
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] SANCTUS ALTYAPI: Oyun ismi ayarlandı!`);
 };
+
+
+//sanctus 
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,21 +1,25 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
+
 
 exports.run = (client, message, params) => {
   
   const embed = new Discord.MessageEmbed()
   .setColor('RANDOM')
-  .setTitle("Ping Değerleri")
-  .addField(":signal_strength: API Gecikmesi:", `**${client.ws.ping}** ms`)
+  .setTitle("SANCTUS YARDIM MENÜSÜ")
+  .addField(".**ping**", `Botun Pingini Gösterir`)
 message.channel.send(embed)
 }
+
+
+
 exports.conf = {//sanctus
   enable: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["y"],
   permLevel: 0
 }
 exports.help = {
-  name: "ping",
-  description: "botun pingini gösterir.",
-  usage: "ping"
+  name: "yardım",
+  description: "botun yardım menüsünü gösterir.",
+  usage: "yardım"
 }//sanctus
