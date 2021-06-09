46f47af51f05e47f5f5838d63c31113142b52863
Glitch (glitch-hello-node) <none>
2021-06-09 18:56:21 +0000

    ⚽️🙆 Checkpoint ./ayarlar.json:32363651/46 ./events/ready.js:32363651/10 ./README.md:32363651/297 ./komutlar/kayıtsız.js:32363651/312

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] SANCTUS ALTYAPI: HER DAİM SİZLERLE`);//BURALARI DEĞİŞTİRE BİLİRSİNİZ 
   client.user.setStatus("dnd");
   //idle : Boşta dnd : rahatsız Etmeyin online : çevrimiçi
- client.user.setActivity("SANCTUS <3 SİZ", { type: "WATCHING" });
+ client.user.setActivity("SANCTUS <3 KAYIT", { type: "WATCHING" });
   // PLAYING : oynuyur LISTENING : dinliyor  WATCHING : İZLİOR 
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] SANCTUS ALTYAPI: Oyun ismi ayarlandı!`);
 };
---
+++ komutlar/kayıtsız.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, params) => {
+ 
+
+
+
+
+}
+  exports.conf = {//sanctus
+  enable: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+}
+exports.help = {
+  name: "ping",
+  description: "botun pingini gösterir.",
+  usage: "ping"
+}//sanctus
