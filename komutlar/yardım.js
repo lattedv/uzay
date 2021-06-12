5ce53a553f372555e3327ef8f6c03167935233d8
Glitch (glitch-hello-node) <none>
2021-06-12 19:04:01 +0000

    🌶🚓 Checkpoint ./komutlar/yardım.js:28863423/4101 ./ayarlar.json:28863423/2

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,45 +1,38 @@
-const Discord = require ("discord.js");
-
-exports.run = (client, message) => {
-const lembed = new Discord.MessageEmbed()
-
-.then;   
-const mhelp = new Discord.MessageEmbed()
-.setColor("#140589")
-.setAuthor(`${client.user.username} `, client.user.avatarURL)  
-.setTitle(":Alarm2: Arrow Bot | Moderasyon Menüsü :Alarm2:")
-.setThumbnail("https://cdn.discordapp.com/emojis/727894683061321759.gif?v=1")
-    .setDescription(`
+const Discord = require("discord.js");
 
 
-╔════════════════════════════════════╗
-║**»** **a!ban** : Sunucudan bir üyeyi yasaklar.
-║**»** **a!unban** : İstediğiniz kişinin banını kaldırır.
-║**»** **a!sil** : Belirli bir kanaldaki mesajları siler.
-║**»** **a!kick** : Sunucudan bir üye kickler.
-║**»** **a!yavaş-mod** : Sohbete yazma sınır (süre) ekler.
-║**»** **a!kilitle** : Kanalı istediğiniz kadar süreyle kitler.
-║**»** **a!kategori-oluştur** : Yeni Bir Kategori Açar.
-║
-║**»** \`\`\a!yardım\`\`\ **__Botun Tüm Komutlarına Ulaşa Bilirsiniz.__**
-║
-╚════════════════════════════════════╝
-`) 
-       .addField(`» Arrow Bot Bağlantıları`, `:ate:  [Bot Davet Linki](https://discord.com/api/oauth2/authorize?client_id=728920362636542032&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/G5CZFT3) **|** [WebSitesi](http://ab.arrow-bot.tk/) :ate:`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz 
-        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
-message.channel.send(mhelp)
-.then; const sembed = new Discord.MessageEmbed()
+exports.run = (client, message, params) => {
+  
+  const embed = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  
+  .setTitle("BOŞ YARDIM MENÜSÜ")
+  .addField("```.e:Belirttiğiniz kullanıcıyı erkek olarak kayıt eder.```")
+  .addField(".**k**", `Belirttiğiniz kullanıcıyı kadın olarak kayıt eder.`) 
+  .addField(".**nuke**", `Kanalı siler ve kopyasını oluşturur.`) 
+  .addField(".**ban**", `Belirttiğiniz üyeyi sunucudan yasaklar.`) 
+  .addField(".**kick**", `Belirttiğiniz üyeyi sunucudan atar.`) 
+  .addField(".**sil**", `Belirttiğiniz sayıda mesajı siler.`) 
+  .addField(".**isim**", `Belirttiğiniz üyenin ismini değiştirir.`) 
+  .addField(".**ping**", `Botun pingini gösterir.`) 
+  .addField(".**say**", `Sunucu hakkında bilgi verir.`) 
+  .addField(".**top**", `Kayıt sıralamasını gösterir.`) 
+  .addField(".**kayıtsız**", `Belirttiğiniz üyenin tüm rollerini alır ve kayıtsız rolü verir.`) 
 
-}; 
+
+message.channel.send(embed)
+}
+
+
+
 exports.conf = {
-    enabled: true, 
-    guildOnly: false, 
-    aliases: ["mod"], 
-    permLevel: 0 
-  };
- 
-  exports.help = {
-    name: 'moderasyon', 
-    description: '',
-    usage: ''
-  };
+  enable: true,
+  guildOnly: false,
+  aliases: ["y"],
+  permLevel: 0
+}
+exports.help = {
+  name: "yardım",
+  description: "botun yardım menüsünü gösterir.",
+  usage: "yardım"
+}
