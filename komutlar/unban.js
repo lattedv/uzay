049b972c09d72240095cfd006cb987efff23c248
Glitch (glitch-hello-node) <none>
2021-06-13 11:50:10 +0000

    🚒🌵 Checkpoint ./komutlar/sil.js:28863423/148 ./komutlar/kick.js:28863423/114 ./komutlar/erkek.js:28863423/9 ./komutlar/stat.js:28863423/37 ./komutlar/unban.js:28863423/92 ./komutlar/kayıttoplam.js:28863423/37

--- komutlar/unban.js
+++ komutlar/unban.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require('discord.js');
 
 exports.run = async (client, message, args) => {
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(new MessageEmbed().setColor("BLUE").setDescription(`! | Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin!`))
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(new MessageEmbed().setColor("BLUE").setDescription(`**\<a:redgif:853591843630219304> | Bu komutu kullanabilmek için gerekli yetkiye sahip değilsin!**`))
     let user = args[0];
     const banList = await message.guild.fetchBans();
     if (!user || isNaN(user) || !banList.has(user)) {
-        return message.channel.send(new MessageEmbed().setColor("BLUE").setTitle("Hata Kullanım ").setDescription(`Hatalı ID Veya Kullanıcı Banlı Değil.`))
+        return message.channel.send(new MessageEmbed().setColor("BLUE").setTitle("Hata Kullanım ").setDescription(`**\<a:redgif:853591843630219304> | Hatalı ID veya kullanıcı banlı değil.**`))
     }
     message.guild.members.unban(user);
-    message.channel.send((new MessageEmbed().setColor("BLUE").setTitle("Başarılı").setDescription(`<@${args[0]}> \`(${args[0]})\` Kullanıcının Yasağı Kaldırıldı. | :tick:`)))
+    message.channel.send((new MessageEmbed().setColor("BLUE").setTitle("Başarılı").setDescription(`<@${args[0]}> \`(${args[0]})\` Kullanıcının yasağı kaldırıldı. | :tick:`)))
 };
 
 exports.conf = {
